8481c09ca1ed1da721fe5062126a5c7ac3413350
sumba101 <sumanth.goku@gmail.com>
2021-05-05 19:45:04 +0530

    testing with randaug

--- dali_utils/dali_transforms.py
+++ dali_utils/dali_transforms.py
@@ -5,10 +5,6 @@ import nvidia.dali.types as types
 from nvidia.dali.pipeline import Pipeline
 from torchvision.datasets import ImageFolder
 
-from models.RandAugment import RandAugment
-
-rander = RandAugment( 5, 5 )
-
 
 class SimCLRTransform( Pipeline ):
     def __init__(self, DATA_PATH, input_height, batch_size, copies, stage, num_threads, device_id, seed=1729):
@@ -32,50 +28,49 @@ class SimCLRTransform( Pipeline ):
         self.blur_amt = ops.random.Uniform( values=[float( i ) for i in range( 1, int( 0.1 * self.input_height ), 2 )] )
 
         self.decode = ops.ImageDecoder( device='mixed', output_type=types.RGB )
-        self.crop = ops.RandomResizedCrop( size=self.input_height, minibatch_size=batch_size, device="gpu",
-                                           dtype=types.FLOAT )
-        self.flip = ops.Flip( vertical=self.coin(), horizontal=self.coin(), device="gpu" )
-        self.colorjit_gray = ops.ColorTwist( brightness=self.uniform(), contrast=self.uniform(), hue=self.uniform(),
-                                             saturation=self.uniform(), device="gpu" )
-        self.blur = ops.GaussianBlur( window_size=self.to_int32_cpu( self.blur_amt() ), device="gpu",
-                                      dtype=types.FLOAT )
-
-        self.swapaxes = ops.Transpose( perm=[2, 0, 1], device="gpu" )
-        self.brightness_contrast=ops.brightness_contrast(brightness=self.uniform(),
-                                                         contrast=self.uniform(),contrast_center=self.uniform())
-        self.hue=ops.hue(hue=self.uniform())
-        self.hsv=ops.hsv(hue=self.uniform(),saturation=self.uniform(),value=self.uniform())
-        self.water=ops.water(ampl_x=self.uniform,ampl_y=self.uniform,fill_value=self.uniform,
-                             freq_X=self.uniform(),freq_y=self.uniform())
-        self.transforms_shear=ops.transforms.shear(angles=[self.uniform(),self.uniform()])
+        # self.crop = ops.RandomResizedCrop( size=self.input_height, minibatch_size=batch_size, device="gpu",
+        #                                    dtype=types.FLOAT )
+        # self.flip = ops.Flip( vertical=self.coin(), horizontal=self.coin(), device="gpu" )
+        # self.colorjit_gray = ops.ColorTwist( brightness=self.uniform(), contrast=self.uniform(), hue=self.uniform(),
+        #                                      saturation=self.uniform(), device="gpu" )
+        # self.blur = ops.GaussianBlur( window_size=self.to_int32_cpu( self.blur_amt() ), device="gpu",
+        #                               dtype=types.FLOAT )
+        #
+        # self.swapaxes = ops.Transpose( perm=[2, 0, 1], device="gpu" )
+        # self.brightness_contrast=ops.brightness_contrast(brightness=self.uniform(),
+        #                                                  contrast=self.uniform(),contrast_center=self.uniform())
+        # self.hue=ops.hue(hue=self.uniform())
+        # self.hsv=ops.hsv(hue=self.uniform(),saturation=self.uniform(),value=self.uniform())
+        # self.water=ops.water(ampl_x=self.uniform,ampl_y=self.uniform,fill_value=self.uniform,
+        #                      freq_X=self.uniform(),freq_y=self.uniform())
+        # self.transforms_shear=ops.transforms.shear(angles=[self.uniform(),self.uniform()])
         self.augment_list = [
-            (ops.RandomResizedCrop( size=self.input_height, minibatch_size=batch_size, device="gpu",
-                                    dtype=types.FLOAT ),),
-            (ops.Flip( vertical=self.coin(), horizontal=self.coin(), device="gpu" ),),
-            (ops.ColorTwist( brightness=self.uniform(), contrast=self.uniform(), hue=self.uniform(),
-                             saturation=self.uniform(), device="gpu" ),),
-            (ops.GaussianBlur( window_size=self.to_int32_cpu( self.blur_amt() ), device="gpu", dtype=types.FLOAT )),
-            (ops.Transpose( perm=[2, 0, 1], device="gpu" )),
-            (ops.brightness_contrast(brightness=self.uniform(),
-                                                         contrast=self.uniform(),contrast_center=self.uniform())),
-            (ops.water(ampl_x=self.uniform,ampl_y=self.uniform,fill_value=self.uniform,
-                             freq_X=self.uniform(),freq_y=self.uniform())),
-            (ops.transforms.shear(angles=[self.uniform(),self.uniform()]))
-
-
+            ops.RandomResizedCrop( size=self.input_height, minibatch_size=batch_size, device="gpu",  # magnitude
+                                   dtype=types.FLOAT ),
+            ops.Flip( vertical=self.coin(), horizontal=self.coin(), device="gpu" ),
+            ops.ColorTwist( brightness=self.uniform(), contrast=self.uniform(), hue=self.uniform(),
+                            saturation=self.uniform(), device="gpu" ),
+            ops.GaussianBlur( window_size=self.to_int32_cpu( self.blur_amt() ), device="gpu", dtype=types.FLOAT ),
+            ops.Transpose( perm=[2, 0, 1], device="gpu" ),
+            ops.brightness_contrast( brightness=self.uniform(),
+                                     contrast=self.uniform(), contrast_center=self.uniform() ),
+            ops.water( ampl_x=self.uniform, ampl_y=self.uniform, fill_value=self.uniform,
+                       freq_X=self.uniform(), freq_y=self.uniform() ),
+            ops.transforms.shear( angles=[self.uniform(), self.uniform()] ),
+            ops.hue( hue=self.uniform() ),
+            ops.hsv( hue=self.uniform(), saturation=self.uniform(), value=self.uniform() )
         ]
 
         # Todo: Increase the list of dali ops
         #       See how to apply magnitude to each accordingly
         #       Run and pray to god
 
-    def rand_aug(self):
-
+    def rand_aug(self, img):
         ops = random.choices( self.augment_list, k=self.n )
-        for op, minval, maxval in ops:
-            val = (float( self.m ) / 30) * float( maxval - minval ) + minval
-            img = op( img, val )
-
+        breakpoint()
+        for op in ops:
+            img = op( img )
+            breakpoint()
         return img
 
     def train_transform(self, image):
@@ -85,12 +80,6 @@ class SimCLRTransform( Pipeline ):
         image = self.colorjit_gray( image )
         image = self.blur( image )
         image = self.swapaxes( image )
-        return image
-
-    def train_rand_transform(self, image):
-        breakpoint()
-        image = rander( image )
-        breakpoint()
         return image
 
     def val_transform(self, image):
@@ -104,7 +93,7 @@ class SimCLRTransform( Pipeline ):
         jpegs = self.decode( jpegs )
         breakpoint()
         if self.stage == 'train':
-            self.transform = self.train_transform  # Todo: concern
+            self.transform = self.rand_aug  # Todo: concern
         else:
             self.transform = self.val_transform
 
