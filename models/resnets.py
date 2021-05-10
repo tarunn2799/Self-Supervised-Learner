00ed3b502f21d93a100d97c5f113379790fb869d
sumba101 <sumanth.goku@gmail.com>
2021-05-10 16:49:25 +0530

    replacing resnet class with lightining bolt resnet class
    Signed-off-by: sumba101 <sumanth.goku@gmail.com>

--- models/resnets.py
+++ models/resnets.py
@@ -132,21 +132,15 @@ class Bottleneck( nn.Module ):
 
 
 class ResNet( nn.Module ):
-    def __init__(
-            self,
-            block,
-            layers,
-            num_classes=10,
-            zero_init_residual=False,
-            groups=1,
-            width_per_group=64,
-            replace_stride_with_dilation=None,
-            norm_layer=None,
-    ):
+
+    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
+                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
+                 norm_layer=None, return_all_feature_maps=False):
         super( ResNet, self ).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
         self._norm_layer = norm_layer
+        self.return_all_feature_maps = return_all_feature_maps
 
         self.inplanes = 64
         self.dilation = 1
@@ -155,38 +149,28 @@ class ResNet( nn.Module ):
             # the 2x2 stride with a dilated convolution instead
             replace_stride_with_dilation = [False, False, False]
         if len( replace_stride_with_dilation ) != 3:
-            raise ValueError(
-                "replace_stride_with_dilation should be None "
-                "or a 3-element tuple, got {}".format( replace_stride_with_dilation )
-            )
+            raise ValueError( "replace_stride_with_dilation should be None "
+                              "or a 3-element tuple, got {}".format( replace_stride_with_dilation ) )
         self.groups = groups
         self.base_width = width_per_group
-
-        # CIFAR10: kernel_size 7 -> 3, stride 2 -> 1, padding 3->1
-        self.conv1 = nn.Conv2d(
-            3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False
-        )
-        # END
-
+        self.conv1 = nn.Conv2d( 3, self.inplanes, kernel_size=3, stride=1, padding=1,
+                                bias=False )
         self.bn1 = norm_layer( self.inplanes )
         self.relu = nn.ReLU( inplace=True )
         self.maxpool = nn.MaxPool2d( kernel_size=3, stride=2, padding=1 )
         self.layer1 = self._make_layer( block, 64, layers[0] )
-        self.layer2 = self._make_layer(
-            block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0]
-        )
-        self.layer3 = self._make_layer(
-            block, 256, layers[2], stride=2, dilate=replace_stride_with_dilation[1]
-        )
-        self.layer4 = self._make_layer(
-            block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2]
-        )
+        self.layer2 = self._make_layer( block, 128, layers[1], stride=2,
+                                        dilate=replace_stride_with_dilation[0] )
+        self.layer3 = self._make_layer( block, 256, layers[2], stride=2,
+                                        dilate=replace_stride_with_dilation[1] )
+        self.layer4 = self._make_layer( block, 512, layers[3], stride=2,
+                                        dilate=replace_stride_with_dilation[2] )
         self.avgpool = nn.AdaptiveAvgPool2d( (1, 1) )
         self.fc = nn.Linear( 512 * block.expansion, num_classes )
 
         for m in self.modules():
             if isinstance( m, nn.Conv2d ):
-                nn.init.kaiming_normal_( m.weight, mode="fan_out", nonlinearity="relu" )
+                nn.init.kaiming_normal_( m.weight, mode='fan_out', nonlinearity='relu' )
             elif isinstance( m, (nn.BatchNorm2d, nn.GroupNorm) ):
                 nn.init.constant_( m.weight, 1 )
                 nn.init.constant_( m.bias, 0 )
@@ -215,49 +199,36 @@ class ResNet( nn.Module ):
             )
 
         layers = []
-        layers.append(
-            block(
-                self.inplanes,
-                planes,
-                stride,
-                downsample,
-                self.groups,
-                self.base_width,
-                previous_dilation,
-                norm_layer,
-            )
-        )
+        layers.append( block( self.inplanes, planes, stride, downsample, self.groups,
+                              self.base_width, previous_dilation, norm_layer ) )
         self.inplanes = planes * block.expansion
         for _ in range( 1, blocks ):
-            layers.append(
-                block(
-                    self.inplanes,
-                    planes,
-                    groups=self.groups,
-                    base_width=self.base_width,
-                    dilation=self.dilation,
-                    norm_layer=norm_layer,
-                )
-            )
+            layers.append( block( self.inplanes, planes, groups=self.groups,
+                                  base_width=self.base_width, dilation=self.dilation,
+                                  norm_layer=norm_layer ) )
 
         return nn.Sequential( *layers )
 
     def forward(self, x):
-        x = self.conv1( x )
-        x = self.bn1( x )
-        x = self.relu( x )
-        x = self.maxpool( x )
+        x0 = self.conv1( x )
+        x0 = self.bn1( x0 )
+        x0 = self.relu( x0 )
+        x0 = self.maxpool( x0 )
 
-        x = self.layer1( x )
-        x = self.layer2( x )
-        x = self.layer3( x )
-        x = self.layer4( x )
+        if self.return_all_feature_maps:
+            x1 = self.layer1( x0 )
+            x2 = self.layer2( x1 )
+            x3 = self.layer3( x2 )
+            x4 = self.layer4( x3 )
 
-        x = self.avgpool( x )
-        x = x.reshape( x.size( 0 ), -1 )
-        x = self.fc( x )
+            return [x0, x1, x2, x3, x4]
+        else:
+            x0 = self.layer1( x0 )
+            x0 = self.layer2( x0 )
+            x0 = self.layer3( x0 )
+            x0 = self.layer4( x0 )
 
-        return x
+            return [x0]
 
 
 def _resnet(arch, block, layers, pretrained, progress, device, **kwargs):
